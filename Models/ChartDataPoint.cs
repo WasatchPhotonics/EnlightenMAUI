273b9d4412a7f456d3c6c59b45c5f8a9d5ec0ef0
Mark Zieg <mark@zieg.com>
2024-04-13 18:10:56 -0400

    added Telerik, started Chart, segfaulting

---
+++ Models/ChartDataPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EnlightenMAUI.Models
+{
+    public class ChartDataPoint
+    {
+        public double xValue { get; set; }
+        public double intensity { get; set; }
+
+        public ChartDataPoint()
+        {
+        }
+    }
+}
