1252ec226be271624c2885e6ed8fd9ff5d51cc24
Trent Stohrer <tstohrer@wasatchphotonics.com>
2024-09-25 08:49:10 -0400

    fixed name

--- ViewModels/ScopeViewModel.cs
+++ ViewModels/ScopeViewModel.cs
@@ -275,7 +275,7 @@ public class ScopeViewModel : INotifyPropertyChanged
         }
     }
 
-    public bool ramanModeEnabled
+    public bool autoDarkEnabled
     {
         get => spec.autoDarkEnabled;
         set
@@ -293,9 +293,9 @@ public class ScopeViewModel : INotifyPropertyChanged
     {
         get
         {
-            var available = !ramanModeEnabled && spec.battery.level >= 5;
+            var available = !autoDarkEnabled && spec.battery.level >= 5;
             if (!available)
-                logger.debug($"laser not available because ramanModeEnabled ({ramanModeEnabled}) or bettery < 5 ({spec.battery.level})");
+                logger.debug($"laser not available because ramanModeEnabled ({autoDarkEnabled}) or bettery < 5 ({spec.battery.level})");
             return available;
         }
     }
@@ -305,7 +305,7 @@ public class ScopeViewModel : INotifyPropertyChanged
         logger.debug("SVM.updateLaserProperties: start");
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(laserIsAvailable)));
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(laserEnabled)));
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ramanModeEnabled)));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(autoDarkEnabled)));
         logger.debug("SVM.updateLaserProperties: done");
     }
 
