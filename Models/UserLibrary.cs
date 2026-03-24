8a212688b499e6d1bfa14574704e777d6d813348
Trent Stohrer <tstohrer@wasatchphotonics.com>
2026-03-24 11:31:09 -0400

    starting in on UserLibrary

---
+++ Models/UserLibrary.cs
@@ -0,0 +1,24 @@
+﻿using EnlightenMAUI.Platforms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnlightenMAUI.Models
+{
+    internal class UserLibrary
+    {
+        Settings settings = Settings.getInstance();
+
+        public async Task loadTodaySpectra()
+        {
+
+            Dictionary<string, Measurement> library = new Dictionary<string, Measurement>();
+            Dictionary<string, double[]> originalRaws = new Dictionary<string, double[]>();
+            Dictionary<string, double[]> originalDarks = new Dictionary<string, double[]>();
+            string path = PlatformUtil.getAutoSavePath(settings.highLevelAutoSave);
+            await PlatformUtil.loadFiles(useAssets: false, path, library, originalRaws, originalDarks, true, null);
+
+        }
+
+    }
+}
