b39c51f31c62d2accaf2f5c3fd4de66c5e6ef9e2
Trent Stohrer <tstohrer@wasatchphotonics.com>
2026-04-22 10:58:57 -0400

    fixed styling for radio button

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -38,6 +38,41 @@ public static class MauiProgram
 
         builder.Services.AddTransient<AppShell>();
 
+        Microsoft.Maui.Handlers.RadioButtonHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        {
+#if ANDROID
+            if (handler.PlatformView is Android.Widget.RadioButton radioButton)
+            {
+                var states = new int[][]
+                {
+                        new int[] { Android.Resource.Attribute.StateChecked },
+                        new int[] { -Android.Resource.Attribute.StateChecked }
+                };
+
+                // Define the colors corresponding to the states (border color)
+                var colors = new int[]
+                {
+                        new Android.Graphics.Color(0x27, 0xc0, 0xa1),
+                        new Android.Graphics.Color(0x27, 0xc0, 0xa1)
+                };
+
+                // Create a ColorStateList with the states and colors
+                var colorStateList = new Android.Content.Res.ColorStateList(states, colors);
+
+                // Apply the color tint to the button's border
+                radioButton.ButtonTintList = colorStateList;
+            }
+#endif
+        });
+
+        /*
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.BorderStyle = Android.Graphics.Color.White;
+#endif
+        });
+        */
 
 #if DEBUG
         builder.Logging.AddDebug();
